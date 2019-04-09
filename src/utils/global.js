9c0d29f5b22e7bb194572b4d42e2975ff5cd8ea0
opeendoing <dyninger@163.com>
2019-04-09 20:54:30 +0800

    图片资源

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,8 +1,8 @@
 const config = {
   // base_url: 'http://106.14.188.43:8004',
-  base_url: 'http://192.168.1.114:8006',
-  image_url: 'http://192.168.1.114:8006/image/',
-  upload_avatar: 'http://192.168.1.114:8006/user/avatar',
+  base_url: 'http://192.168.1.107:8006',
+  image_url: 'http://192.168.1.107:8006/image/',
+  upload_avatar: 'http://192.168.1.107:8006/user/avatar',
   selected: '约伴'
 }
 
